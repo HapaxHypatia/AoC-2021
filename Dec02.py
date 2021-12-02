6f8d7a62749f94860bff99ed5b8400b747bf1892
HapaxHypatia <r.haskmann@gmail.com>
2021-12-02 16:38:56 +1000

    Part 2 first pass

--- Dec02.py
+++ Dec02.py
@@ -10,10 +10,29 @@ def part01(data):
         distance = int(item[-1])
         match direction:
             case "forward": x += distance
-            case "back": x -= distance
             case "up": y -= distance
             case "down": y += distance
     return x*y
 
 
+def part02(data):
+    x = 0
+    y = 0
+    aim = 0
+
+    for item in data:
+        direction = item[:-2]
+        distance = int(item[-1])
+        match direction:
+            case "forward":
+                x += distance
+                y += aim*distance
+            case "up":
+                aim -= distance
+            case "down":
+                aim += distance
+    return x*y
+
+
 print(part01(moves))
+print(part02(moves))
